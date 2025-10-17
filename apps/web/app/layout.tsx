7622235d12cc2aca4e0a102432e7095b0843528c
Rushi0508 <rushigandhi14@gmail.com>
2025-10-17 14:03:40 +0530

    feat(layout): add Umami analytics script to the root layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,10 +5,11 @@ import { Metadata } from 'next';
 import { ReactQueryClientProvider } from '@/lib/tanstack-provider';
 import { dmSans } from '@/lib/fonts';
 import GlobalLoading from './loading';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: 'Founders Form',
-  description: 'Founders Form',
+  title: 'Karmi',
+  description: 'Karmi - Your Vedic Astrologer',
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <ReactQueryClientProvider>
       <html lang='en' suppressHydrationWarning>
+        <Script defer src='https://cloud.umami.is/script.js' data-website-id='bdf37123-ccfa-464e-b528-0bb9426a15ed' />
         <body className={`${dmSans.className} antialiased`}>
           <Suspense fallback={<GlobalLoading />}>
             <Providers>{children}</Providers>
